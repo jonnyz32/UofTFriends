052aa06811fd259c8cc36676dc329b6f2d1f1ab7
AdiThakur <55326963+AdiThakur@users.noreply.github.com>
2020-11-08 11:45:50 -0500

    Updated change bio settings

--- src/components/SettingsPage/SettingsPage.js
+++ src/components/SettingsPage/SettingsPage.js
@@ -6,16 +6,23 @@ import logo from '../../images/Mobsquare.png'
 
 export class SettingsPage extends React.Component {
 
-	
+
 	state = {
 		currentUser: this.props.currentUser,
 		chats: this.props.chats,
 		newCourse: "",
+		updatedBio: ""
+	}
+
+	courseOnChange = (event) => {
+		// this.newCourse = event.target.value.trim()
+		this.setState({ newCourse: event.target.value.trim().toUpperCase() })
 	}
 
 	addCourse = () => {
 
-		if (this.state.chats.includes(this.state.newCourse) || (this.state.newCourse == "")) {
+		if (this.state.chats.includes(this.state.newCourse) || (this.state.newCourse == "") || 
+				(this.state.currentUser.courses.length >= 6)) {
 			return
 		}
 
@@ -25,7 +32,7 @@ export class SettingsPage extends React.Component {
 		let newChats = this.state.chats
 		newChats.push(this.state.newCourse)
 
-		this.setState({currentUser: updatedUser, chats: newChats, newCourse: ""})
+		this.setState({ currentUser: updatedUser, chats: newChats, newCourse: "" })
 	}
 
 	removeCourse = (event) => {
@@ -40,25 +47,39 @@ export class SettingsPage extends React.Component {
 		let newChats = this.state.chats
 		newChats.splice(chatIndex, 1)
 
-		this.setState({currentUser: updatedUser, chats: newChats})
+		this.setState({ currentUser: updatedUser, chats: newChats })
 	}
 
-	courseOnChange = (event) => {
-		// this.newCourse = event.target.value.trim()
-		this.setState({newCourse: event.target.value.trim().toUpperCase()})
+	bioOnChange = (event) => {
+		this.setState({ updatedBio: event.target.value.trim()})
+	}
+
+	submitBio = () => {
+
+		if (this.state.updatedBio == "") {
+			return
+		}
+
+		let updatedUser = this.state.currentUser
+		updatedUser.bio = this.state.updatedBio
+		this.setState({currentUser: updatedUser})
 	}
 
 	render() {
 
 		return (
 			<div className="SettingsRoot">
-				
+
 				<div className="SettingsHeaderContainer">
 					<img className="SettingsAvatar" src={logo}></img>
 					<p className="SettingsName"> {this.state.currentUser.name} </p>
 				</div>
 
 				<div className="SettingsBodyContainer">
+
+					<div className="SettingsScheduleContainer">
+						Schedule and shit.
+					</div>
 
 					<div className="SettingsStudentInfo">
 
@@ -70,18 +91,14 @@ export class SettingsPage extends React.Component {
 							<form className="SettingsAddCourseForm" onChange={this.courseOnChange}>
 								<input placeholder="Enter new course..." value={this.state.newCourse}></input>
 							</form>
-							<button className="SettingsAddCourseButton"  onClick={this.addCourse}>Add</button>
+							<button className="SettingsAddCourseButton" onClick={this.addCourse}>Add</button>
 						</div>
-						
-						<form className="SettingsBioContainer">
+
+						<form className="SettingsBioContainer" onChange={this.bioOnChange}>
 							<textarea className="SettingsBio">{this.state.currentUser.bio}</textarea>
 						</form>
 
-						<button className="SettingsSubmit">Save</button>
-					</div>
-
-					<div className="SettingsScheduleContainer">
-						Schedule and shit.
+						<button className="SettingsSubmitBio" onClick={this.submitBio}>Update Bio</button>
 					</div>
 
 				</div>
