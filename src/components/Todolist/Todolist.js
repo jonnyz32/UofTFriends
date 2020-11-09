f7425fb8773db4c047b8c9588380f78e6bd7a00b
Shadman Aziz <97sdmn@gmail.com>
2020-11-09 03:26:09 -0500

    fixed bug on todolist from previous commit

--- src/components/Todolist/Todolist.js
+++ src/components/Todolist/Todolist.js
@@ -4,67 +4,70 @@ import './Todolist.css';
 
 class ToDoList extends Component {
 
-	constructor(props) {
-		super(props)
-		this.keyCount = 0
-	}
+  constructor(props){
+    super(props)
+    this.keyCount = 0
+  }
 
-	state = {
-		time: "",
-		todoList: this.props.todoList,
-		activity: ""
-	}
+   state = {
+     time: "",
+     todoList: this.props.todoList,
+     activity:""
 
-	newKey = () => {
-		this.keyCount += 1
-		return this.keyCount;
-	}
-
-	handleInputChange = (event) => {
-		const target = event.target
-		const value = target.value
-		const name = target.name
-
-		this.setState({
-			[name]: value
-		})
-	}
-
-	addNewTodo = () => {
-		const newTodoList = this.state.todoList
-		const newTodo = { activity: this.state.activity, time: this.state.time }
-		newTodoList.push(newTodo)
-		this.setState({
-			todolist: this.props.todolist
-		});
-
-		console.log(this.state.toDoList)
-	}
+   }
 
 
 
-	render() {
-		return (
-			<div className="Todolist">
-				<h2> To do list </h2>
+  newKey = () => {
+    this.keyCount += 1
+    return this.keyCount;
+  }
 
-				<div>
-					{this.props.todoList.map((toDoListItem) => 
-						<ToDoListItem key={this.newKey()}>
-							name={this.props.name} 
-							activity={toDoListItem.activity}
-							time={toDoListItem.time}
-						</ToDoListItem>
-					)}
-				</div>
+   handleInputChange = (event) => {
+     const target = event.target
+     const value = target.value
+     const name=target.name
 
-				<input name="activity" type='text' value={this.state.activity} onChange={this.handleInputChange} placeholder='new task' />
-				<input name="time" type='text' value={this.state.time} onChange={this.handleInputChange} placeholder='time for new task' />
-				<button className="button" onClick={this.addNewTodo}> add new ToDo </button>
-			</div>
+     this.setState({
 
-		);
-	}
+       [name]:value
+     }
+
+     )
+   }
+
+   addNewTodo = () => {
+     const newTodoList = this.state.todoList
+     const newTodo = {activity: this.state.activity, time: this.state.time}
+     newTodoList.push(newTodo)
+     this.setState ({
+       todolist : this.props.todolist
+     });
+
+     console.log(this.state.toDoList)
+
+   }
+
+
+
+  render() {
+
+    return (
+      <div className="Todolist">
+      <h2> To do list </h2>
+      <div>
+
+     {this.props.todoList.map((toDoListItem) => <ToDoListItem key={this.newKey()} name={this.props.name} activity={toDoListItem.activity}
+                                                                   time={toDoListItem.time}
+                                                                   />)}
+      </div>
+      <input name="activity"  type='text' value= {this.state.activity} onChange={this.handleInputChange} placeholder='new task'/>
+      <input name="time"  type='text' value= {this.state.time} onChange={this.handleInputChange} placeholder='time for new task'/>
+      <button className="button" onClick={this.addNewTodo}> add new ToDo </button>
+      </div>
+
+    );
+  }
 }
 
 export default ToDoList;
