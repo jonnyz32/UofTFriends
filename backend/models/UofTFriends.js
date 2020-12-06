5d6fd64a33e83c7261220a819335a21e5328ebf6
AdiThakur <55326963+AdiThakur@users.noreply.github.com>
2020-12-05 19:12:09 -0500

    Added Student, ToDoList, and Course models.

--- backend/models/UofTFriends.js
+++ backend/models/UofTFriends.js
@@ -1,23 +1,56 @@
-/*Models */
-
 const mongoose = require('mongoose');
 
 const Message = new mongoose.Schema({
     sender: String,
     text: String,
-});
+})
 
-const GroupsSchema = new mongoose.Schema({
+const Group = new mongoose.Schema({
     name: String,
-    members: [{studentId: String}],
+    members: [{ studentId: String }],
     messages: [Message]
-});
+})
 
+const ToDoList = new mongoose.Schema({
+    activity: String,
+    time: Date
+})
 
+const CourseMeeting = new mongoose.Schema({
+    meetingDay: String,
+    durationMins: Int32
+})
 
-// Reservations will be embedded in the Restaurant model
+const CourseModel = new mongoose.Schema({
+    courseId: String,
+    meetings: [CourseMeeting]
+})
 
+const StudentModel = new mongoose.Schema({
 
-const Group = mongoose.model('Group', GroupsSchema);
+    username: String,
+    password: String,
+    name: String,
 
-module.exports = { Group };
+    // Maybe swap id strings with ObjectIds?
+    // Stores ids to the enrolled courses.
+    courses: [String],
+    program: [String],
+    year: Number,
+    hobbies: [String],
+    todoList: [ToDoList],
+    bio: String,
+    profilePic: String,
+    // Stores ids to the enrolled groups.
+    groups: [String],
+
+    // If a brand new user.
+    seenOnboarding: false,
+    seenTutorial: false,
+})
+
+const Group = mongoose.model('Group', Group);
+const Student = mongoose.model('Student', StudentModel)
+const Course = mongoose.model('Course', CourseModel)
+
+module.exports = { Group, Student, Course };
