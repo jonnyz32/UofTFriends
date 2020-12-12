804b0738562dbeaa8fd526d9f729a289a27cc5c9
Meirbek Zeinulla <meirbekzt@yahoo.com>
2020-12-11 21:29:04 -0500

    changed mongoose uri

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 // DO NOT CHANGE THIS FILE
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/UofTFriends', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Jonathan:Team02@cluster0.tn7m4.mongodb.net/UofTFriends?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 // DO NOT CHANGE THIS FILE
 module.exports = { mongoose }
