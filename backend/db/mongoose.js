6292783068d801414d66b96e7d1f3e1e94d665bd
Meirbek Zeinulla <meirbekzt@yahoo.com>
2020-12-11 21:14:32 -0500

    removed old files

--- backend/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-// DO NOT CHANGE THIS FILE
-const mongoose = require('mongoose');
-
-// DO NOT CHANGE THIS FILE
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/UofTFriends', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-
-// DO NOT CHANGE THIS FILE
-module.exports = { mongoose }
