4fac29a4fac4811c2646378d9d9254054ceecb98
Meirbek Zeinulla <meirbekzt@yahoo.com>
2020-12-11 21:22:24 -0500

    Changed paths

--- server.js
+++ server.js
@@ -289,7 +289,7 @@ app.get('/Courses/:id', async (req, res) => {
 
 /*** Webpage routes below **********************************/
 // Serve the build
-app.use(express.static(path.join(__dirname, "/../client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 // All routes other than above will go to index.html
 app.get("*", (req, res) => {
@@ -301,7 +301,7 @@ app.get("*", (req, res) => {
 	}
 
 	// send index.html
-	res.sendFile("/index.html", { root: '/client/build' });
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 
